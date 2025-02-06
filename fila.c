abbc941be40f4cc68510bba8c0ead66393944d41
Caio3108 <oiacoliveiramuniz@gmail.com>
2025-02-06 20:55:14 -0300

    melhoramento

--- fila.c
+++ fila.c
@@ -2,25 +2,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-Amigo *criação(Amigo **inicio){
-    (*inicio)->proximo = NULL;
-    (*inicio)->anterior = NULL;
+Amigo *criação(Amigo *inicio){
+    inicio = malloc(sizeof(Amigo));
+    inicio->proximo = NULL;
+    inicio->anterior = NULL;
+    return inicio;
 }
 
-int testVazia(Amigo **final) {
+int testVazia(Amigo *final) {
     int r = 0;
-    if (*final == NULL)
+    if (final == NULL)
     {
         r = 1;
     }
     return r;
 }
 
-Amigo *Enfileirar(Amigo **final){
+Amigo *Enfileirar(Amigo *final, char *entrada){
+    if (final == NULL)
+    {
+        final = criação(final);
+    }
+    
     Amigo *nFinal = NULL;
-    nFinal = malloc(sizeof(Amigo));
-    fprintf( , "Nome Idade Compromisso\n");  // concertar
-    fscanf( , "%s %d %s", nFinal->nome, nFinal->idade, nFinal->compromisso);
+    nFinal = malloc(sizeof(Amigo)); 
+    FILE *fs = NULL;
+    fs = fopen(entrada, "r");
+    if (fs == NULL)
+    {
+        printf("erro\n");
+        return;
+    }
     
-    
+
+    fscanf(fs , "%s %d %s", nFinal->nome, &nFinal->idade, nFinal->compromisso); // add campos
+    fclose(fs);
+    final->proximo = nFinal;
+    nFinal->anterior = final;
+    final = nFinal;
+    return final;
 }
